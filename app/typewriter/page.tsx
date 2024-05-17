ae92ff55069183c16fc449e7d0405cdddb00502c
Claudia <cmjengelsman@gmail.com>
2024-05-18 08:29:31 +1200

    Add variable delay based on how much typing is being added

--- app/typewriter/page.tsx
+++ app/typewriter/page.tsx
@@ -4,13 +4,21 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [typed, setTyped] = useState("");
   const [toType, setToType] = useState("");
+  const [delay, setDelay] = useState(100);
 
   const handleChange = (event: any) => {
     setToType(toType + event?.target.value);
   };
 
   useEffect(() => {
-    const delay = 1000;
+    const delayTimer = setTimeout(() => {
+      setDelay(Math.min(1100, toType.length * 50 + 100));
+    }, 200);
+
+    return () => clearTimeout(delayTimer);
+  }, [typed]);
+
+  useEffect(() => {
     const timer = setTimeout(() => {
       setTyped(typed + toType.charAt(0));
       setToType(toType.substring(1));
@@ -22,7 +30,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Think before you type</h1>
+      <h1 className="font-prata">Think first</h1>
       <p>
         A slow typewriter for those who find themselves editing while writing
       </p>
