64e8a2733e814a14fc6ec896b469cd0a7105ede4
Claudia <cmjengelsman@gmail.com>
2024-05-18 18:04:13 +1200

    Finish desktop layout and styling homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-center px-16 md:px-48 py-24">
+    <main className="flex flex-col min-h-screen items-center px-16 md:px-48 py-24 text-brown-900">
       <div className="flex flex-row md:gap-4">
         <div className="w-full flex flex-col justify-center font-lato">
-          <h1 className="text-5xl my-4 text-brown-900 font-prata tracking-wider max-w-2xl mb-8">
+          <h1 className="text-5xl my-4 font-prata tracking-wider max-w-2xl mb-8">
             Think first
           </h1>
+          <h2 className="font-prata text-3xl mb-8">
+            <span className="border-b border-solid border-current w-72 inline-block relative bottom-3"></span>{" "}
+            Write second
+          </h2>
           <p className="mb-4 max-w-2xl">
             Charles Bukowski, the celebrated poet and author belief in
             spontaneity and authenticity led him to famously advise, "Don't
@@ -17,10 +21,9 @@ export default function Home() {
           <p className="mb-4 max-w-2xl">
             Here we embrace Bukowski's philosophy by encouraging writers to
             express their thoughts without the crutch of editing. Here, you
-            can't delete what you've written, and typing to fast will make it go
-            slower, urging you to think carefully before you type. This approach
-            fosters introspective writing, helping you capture the true essence
-            of your thoughts.
+            can't delete what you've written, urging you to think carefully
+            before you type. This approach fosters introspective writing,
+            helping you capture the true essence of your thoughts.
           </p>
           {/* <p>
             Charles Bukowski, a renowned poet and author, was celebrated for his
@@ -49,7 +52,7 @@ export default function Home() {
             Join us in celebrating the spirit of Bukowski and discover the power
             of raw, unfiltered expression.
           </p> */}
-          <button className="w-full font-bold font-lato py-2 px-4 rounded-full my-8 shadow-button-shade-combi shadow-spacebar active:shadow-spacebar-press max-w-2xl">
+          <button className="w-full font-bold font-lato py-2 px-4 rounded-full my-8 shadow-button-shade-combi shadow-spacebar active:shadow-spacebar-press active:translate-x-px max-w-2xl">
             Start writing
           </button>
         </div>
@@ -61,28 +64,50 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="flex flex-col md:flex-row w-full justify-between mt-16">
-        <div className="flex flex-col">
-          <h2 className="font-prata">one thing</h2>
-          <p>
-            Charles Bukowski, the celebrated poet and author, was known for his
-            raw, unfiltered writing style and his unapologetic approach to life.
-            His work, characterized by honesty and grit, rejected traditional
-            literary norms in favor of capturing life as it is—unvarnished and
-            real.
+      <div className="flex flex-col md:flex-row w-full justify-between mt-16 font-lato gap-7 text-center">
+        <div className="flex flex-col w-full items-center">
+          <h2 className="font-prata text-xl mb-4">Charles Bukowski</h2>
+          <p className="text-sm leading-relaxed">
+            the celebrated poet and author, was known for his raw, unfiltered
+            writing style and his unapologetic approach to life. His work,
+            characterized by honesty and grit, rejected traditional literary
+            norms in favor of capturing life as it is—unvarnished and real.
           </p>
         </div>
-        <div className="flex flex-col">
-          <h2 className="font-prata">two thing</h2>
+        <div className="flex flex-col w-full items-center">
+          <h2 className="font-prata text-xl mb-4">Your best work</h2>
+          <p className="text-sm leading-relaxed">
+            Write your best work with supporting options and ignore the things
+            that hold you back.
+          </p>
         </div>
-        <div className="flex flex-col">
-          <h2 className="font-prata">three thing</h2>
+        <div className="flex flex-col-reverse w-full items-center">
+          <h2 className="font-prata text-xl mt-4">- Charles Bukowski</h2>
+          <p className="text-sm leading-relaxed">
+            "An intellectual says a simple thing in a hard way. An artist says a
+            hard thing in a simple way."
+          </p>
+          <p className="text-sm leading-relaxed">And</p>
+          <p className="text-sm leading-relaxed">
+            "Some people never go crazy. What truly horrible lives they must
+            lead."
+          </p>
         </div>
-        <div className="flex flex-col">
-          <h2 className="font-prata">four thing</h2>
+        <div className="flex flex-col w-full items-center">
+          <h2 className="font-prata text-xl mb-4">Reset</h2>
+          <p className="text-sm leading-relaxed">
+            Made with love for the age where everything is permanently being
+            edited. Making you think before you write, or risk having to start
+            over...
+          </p>
         </div>
-        <div className="flex flex-col">
-          <h2 className="font-prata">five thing</h2>
+        <div className="flex flex-col w-full items-center">
+          <h2 className="font-prata text-xl mb-4">Say hi!</h2>
+          <p className="text-sm leading-relaxed">
+            This is a project made for fun, but if it is helping you, or it
+            could help you better with a tweak. I'm happy to hear from you! You
+            can reach me at{" "}
+          </p>
         </div>
       </div>
     </main>
